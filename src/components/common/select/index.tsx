2df7915cf6a9b381a3de18989d596817360962b9
cleisonmp <cleison.mp@gmail.com>
2022-12-05 21:35:41 -0300

    feat: add form support :sparkles:

--- src/components/common/select/index.tsx
+++ src/components/common/select/index.tsx
@@ -1,6 +1,10 @@
+import { useId } from 'react'
 import createCache from '@emotion/cache'
 import { CacheProvider } from '@emotion/react'
+import { Controller, useFormContext } from 'react-hook-form'
 import ReactSelect from 'react-select/creatable'
+import { components } from 'react-select'
+import type { MultiValueGenericProps } from 'react-select'
 
 //in order to allow tailwind classes to have higher priority over react-select defaults
 const cache = createCache({
@@ -18,18 +22,57 @@ export interface SelectOptions {
 interface SelectProps {
   options: SelectOptions[]
   isLoading?: boolean
+  //register: UseFormRegister
+  /*onChange?: string
+  value?: string
+  ref?: string*/
+}
+
+const MultiValueLabel = ({ children, ...props }: MultiValueGenericProps) => {
+  return (
+    <components.MultiValueLabel {...props}>
+      <span
+        onDoubleClick={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          console.log('call edit tag modal')
+        }}
+      >
+        {children}
+      </span>
+    </components.MultiValueLabel>
+  )
 }
 
 export const Select = ({ options, isLoading }: SelectProps) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useFormContext()
+
+  const instaceId = useId()
+
   return (
     <CacheProvider value={cache}>
-      <ReactSelect
-        isMulti
-        isLoading={isLoading}
-        options={options}
-        placeholder='Select or create a category...'
-        classNames={{ control: () => 'p-2' }}
+      <Controller
+        name='tags'
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <ReactSelect
+            {...field}
+            instanceId={instaceId}
+            isMulti
+            isLoading={isLoading}
+            options={options}
+            components={{ MultiValueLabel }}
+            placeholder='Select or create a category...'
+            classNames={{ control: () => 'p-2' }}
+          />
+        )}
       />
+      {errors.tags && <p className='errorMsg'>This is a required field.</p>}
     </CacheProvider>
   )
 }
